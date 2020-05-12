5973f244eee0cc9ea28429eb914bf10b43176518
pedrosilva <pedrogdsilva410@gmail.com>
2020-05-12 17:52:16 +0100

    recreated the repo

---
+++ src/pages/About/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const About = () => {
+  return (
+    <main>
+      <h1>about</h1>
+      <p>some random text and here comes the image</p>
+    </main>
+  );
+};
+
+export default About;
